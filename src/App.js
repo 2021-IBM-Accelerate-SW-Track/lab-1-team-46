7380465c36d1e79181d578e29b632ee8ef6f7ed5
Lingyu-Wei <84238715+Lingyu-Wei@users.noreply.github.com>
2021-06-18 13:51:21 -0500

    06/18/2021
    Added a few components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./component/header"
+import ToDoList from "./component/toDoList"
+import Items from "./component/items"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <ToDoList/>
+    <Items/>
     </div>
   );
 }
