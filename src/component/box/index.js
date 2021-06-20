7e0f9e7d6eed413e9fbb9076612f1e7f61d817a9
Phil-Eggtree <84200961+Phil-Eggtree@users.noreply.github.com>
2021-06-20 12:21:31 -0500

    Created new CSS class rowText for any text that should have elements on the same line.

--- src/component/box/index.js
+++ src/component/box/index.js
@@ -7,12 +7,15 @@ export default function Box(props)
     return( 
     <div>
         <div> {/*This div is ABOVE the box object - just a basic title header on the left side and the add/delete buttons on the right*/}
-            <h2 className = "title">{props.boxTitle}</h2>
+            <h2 className = "rowText" style = {{marginRight: "70%"}}>{props.boxTitle}</h2>
             <button>Add</button>
             <button>Delete</button>
         </div>
 
         <div className = "box">
+            <p className = "rowText" style = {{marginRight:"20%"}}>Task</p>
+            <p className = "rowText" style = {{marginLeft:"20%"}}>Date Added</p>
+            <hr/>
             <Row/>
         </div>
     </div>
