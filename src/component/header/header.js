e1130be6d24226424e549eae5d47738821622c2a
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2021-06-21 18:39:53 -0500

    delete task buttton added to header

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,3 +1,28 @@
 export default function Header(){
     return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
 }
+
+
+deleteTask(e) {
+  if (this._inputElement.value !== "") {
+      var taskToDelete = {
+          taskDescription: this._inputElement.value,
+          timeAdded: Date().toLocaleString()
+      };
+      
+
+      //line to keep track of todo list index
+      const index = this.items.indexOf(taskToDelete);
+   
+      this.setState((prevState) => {
+          return { 
+              items: prevState.items.splice(index,1); 
+          };
+      });
+     
+      this._inputElement.value = null;
+  }
+       
+  e.preventDefault();
+
+}
