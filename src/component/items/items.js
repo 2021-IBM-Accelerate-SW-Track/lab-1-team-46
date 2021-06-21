88ae2cbe82f02e99934b22bcff6c143ab085a18f
michaelahorn97 <55221883+michaelahorn97@users.noreply.github.com>
2021-06-21 18:55:23 -0500

    added add task function and put it in the check duplicates function

---
+++ src/component/items/items.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+ 
+class Items extends Component {
+  addNewTask(item) {
+    return <ul key={item.key}>{item.taskDescription}{item.timeAdded}</ul>
+  }
+ 
+  render() {
+    var addedTask = this.props.entries;
+    var taskList = addedTask.map(this.addNewTask);
+ 
+    return (
+      <ul className="theList">
+          {taskList}
+      </ul>
+    );
+  }
+};
+ 
+export default Items;
