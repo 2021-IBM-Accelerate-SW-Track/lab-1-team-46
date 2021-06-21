28eef3eecf8678e4396cc65cf78ca61eaacdbee5
Phil-Eggtree <84200961+Phil-Eggtree@users.noreply.github.com>
2021-06-21 17:32:15 -0500

    Reset the table to a basic table function with a passed rows array

--- src/component/box/index.js
+++ src/component/box/index.js
@@ -1,20 +1,36 @@
 import './box.css';
 import Row from '../row' 
-
+import TextField from "@material-ui/core/TextField";
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 
 export default function Box(props)
 {
+    function createData(name, date) 
+    {
+        return { name, date};
+    }
+
+    function addRow(rowArray)
+    {
+        rowArray.push(createData(<TextField id="standard-basic" label="Task Description" />, Date().toLocaleString()));
+        console.log(rowArray.length);
+    }
+
     <link rel="stylesheet" href="box.css"></link>
+    const genericRows = [createData(<TextField id="standard-basic" label="Task Description" />, Date().toLocaleString())];
+
+    const[showRows,updateRowHandler] = useState(true);
     return( 
     <div>
         <div> {/*This div is ABOVE the box object - just a basic title header on the left side and the add/delete buttons on the right*/}
             <h2 className = "rowText" style = {{marginRight: "70%"}}>{props.boxTitle}</h2>
-            <button>Add</button>
+            <button onClick = {() => addRow(genericRows)}>Add</button>
             <button>Delete</button>
         </div>
 
         <div className = "box">
-            <Row />
+            <Row rows = {genericRows} createData = {createData} />
         </div>
     </div>
     );
