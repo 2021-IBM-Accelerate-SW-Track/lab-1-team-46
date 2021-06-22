c41369dc297ef8b8e31052c1cf8a0e4394653a23
Phil-Eggtree <84200961+Phil-Eggtree@users.noreply.github.com>
2021-06-21 19:05:14 -0500

    Made it possible to add rows through the add button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     <Header/>
-    <Box boxTitle = "To-do List"/>
+    <Box/>
     </div>
   );
 }
--- src/component/box/index.js
+++ src/component/box/index.js
@@ -1,37 +1,48 @@
 import './box.css';
 import Row from '../row' 
 import TextField from "@material-ui/core/TextField";
-import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import React, { Component } from "react";
 
-export default function Box(props)
-{
-    function createData(name, date) 
+function createData(name, date) 
     {
         return { name, date};
     }
 
-    function addRow(rowArray)
+class Box extends Component {
+
+    constructor(props) {
+        super(props);
+        this.genericRows = [createData(<TextField id="standard-basic" label="Task Description" />, Date().toLocaleString())];
+        
+    }
+
+    addRow(rowArray,object)
     {
         rowArray.push(createData(<TextField id="standard-basic" label="Task Description" />, Date().toLocaleString()));
         console.log(rowArray.length);
+        this.setState(() => {
+            console.log('setting state');
+            return { unseen: "does not display" }
+        });
     }
 
-    <link rel="stylesheet" href="box.css"></link>
-    const genericRows = [createData(<TextField id="standard-basic" label="Task Description" />, Date().toLocaleString())];
 
-    const[showRows,updateRowHandler] = useState(true);
-    return( 
-    <div>
-        <div> {/*This div is ABOVE the box object - just a basic title header on the left side and the add/delete buttons on the right*/}
-            <h2 className = "rowText" style = {{marginRight: "70%"}}>{props.boxTitle}</h2>
-            <button onClick = {() => addRow(genericRows)}>Add</button>
-            <button>Delete</button>
-        </div>
-
-        <div className = "box">
-            <Row rows = {genericRows} createData = {createData} />
-        </div>
-    </div>
-    );
+    render() {
+        return (
+            <div>
+            <div> {/*This div is ABOVE the box object - just a basic title header on the left side and the add/delete buttons on the right*/}
+                <h2 className = "rowText" style = {{marginRight: "70%"}}>"To do List"</h2>
+                <button onClick = {() => this.addRow(this.genericRows)}>Add</button>
+                <button>Delete</button>
+            </div>
+    
+            <div className = "box">
+                <Row rows = {this.genericRows} createData = {createData} />
+            </div>
+           </div>
+        );
+    }
 }
+ 
+export default Box;
